4f4d428008e75bf2e278abd0b6f37e5331a2c3d1
Vinoth <vinoth@Vinoth-MacBook-Pro.local>
2022-12-14 16:04:47 +0530

    End of section 11 fixing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFountComponent } from './core/not-fount/not-fount.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  // {path: 'shop', component: ShopComponent},
-  // {path: 'shop/:id', component: ProductDetailsComponent},
+  {path: 'test-error', component: TestErrorComponent},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: 'not-fount', component: NotFountComponent},
+
   {path: 'shop', loadChildren:()=> import('./shop/shop.module').then(mod => mod.ShopModule)},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
