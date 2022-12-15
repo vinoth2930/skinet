ccb5eb484942abb1e9f78a3a45346eba4458446f
Vinoth <vinoth@Vinoth-MacBook-Pro.local>
2022-12-15 14:23:27 +0530

    End of Section 11

--- client/src/app/core/interceptors/error,interceptor.ts
+++ client/src/app/core/interceptors/error,interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
+import { NavigationExtras, Router } from "@angular/router";
 import { ToastrService } from "ngx-toastr";
 import { catchError, Observable, throwError } from "rxjs";
 
@@ -17,8 +17,11 @@ export class ErrorInterceptor implements HttpInterceptor{
                 if (error){
 
                     if(error.status === 400){
-                        this.toastr.error(error.error.message, error.error.statusCode);
-
+                        if (error.error.errors){
+                            throw error.error;
+                        } else {
+                            this.toastr.error(error.error.message, error.error.statusCode);
+                        }
                     }
 
                     if(error.status === 401){
@@ -32,7 +35,8 @@ export class ErrorInterceptor implements HttpInterceptor{
                         this.router.navigateByUrl('/not-fount');
                     }
                     if (error.status === 500){
-                        this.router.navigateByUrl('/server-error')
+                        const navigationExtras: NavigationExtras = {state: {error: error.error}};
+                        this.router.navigateByUrl('/server-error', navigationExtras);
                     }
                 }
                 return throwError(error);
